7a75cca6c93ba11c1990a010b391b73229bfc631
Suvam Adhikary <adhikary.saheb408@gmail.com>
2021-12-14 21:45:50 +0530

    mongodb comnnect

--- src/configs/db.js
+++ src/configs/db.js
@@ -5,5 +5,5 @@ require('dotenv').config();
 const { DB_PASSWORD } = process.env;
 
 module.exports = () => {
-    return mongoose.connect(`mongodb+srv://CapdaS:${DB_PASSWORD}@capdas.normt.mongodb.net/CapdaSDB?retryWrites=true&w=majority`);
+    return mongoose.connect(`mongodb+srv://Phoenix:${DB_PASSWORD}@cluster0.bpkoe.mongodb.net/dineoutDB?retryWrites=true&w=majority`);
 };
